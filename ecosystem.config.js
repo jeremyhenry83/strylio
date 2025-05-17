be236cfb79d69fb85f1863e04643e533e56f2458
Jeremy <jeremy@strylio.com>
2025-05-17 09:49:53 +0000

    chore: resolution du conflit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-  apps: [
-    {
-      name: 'strylio-dev',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-      env: {
-        PORT: 3001,
-        NODE_ENV: 'production',
-      },
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-    },
-  ],
-}; 
+module.exports = { apps: [{ name: "dev-strylio", script: "node_modules/next/dist/bin/next", args: "start", cwd: "/var/www/dev.strylio.com", env: { PORT: 3000, NODE_ENV: "production" }, instances: 1, autorestart: true, watch: false, max_memory_restart: "1G" }] }
